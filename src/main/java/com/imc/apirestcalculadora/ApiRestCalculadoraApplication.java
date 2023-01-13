bb2d8fc5a9e0300e4597d3a9b6fa997fe243393c
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-01-12 23:45:57 -0300

    Criação do projeto :cat:

---
+++ src/main/java/com/imc/apirestcalculadora/ApiRestCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.imc.apirestcalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imc/apirestcalculadora/ApiRestCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imc.apirestcalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
